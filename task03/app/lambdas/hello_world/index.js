967d4940fda932c10e52a6cc4114b1308fd9818b
Adam Kruppa <adam_kruppa@epam.com>
2025-02-07 15:09:56 +0100

    init

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            statusCode: 200,
+            message: 'Hello from Lambda'
+        })
+    };
+
+    return response;
+};
