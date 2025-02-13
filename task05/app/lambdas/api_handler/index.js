84f222819b4b4fc9993fa647b1469276f4f2000f
Adam Kruppa <adam_kruppa@epam.com>
2025-02-13 13:51:57 +0100

    logs

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -13,10 +13,10 @@ exports.handler = async (event) => {
 
     let data;
 
-    // Check if invoked via API Gateway with a body
+
     if (event.body) {
         try {
-            // Check if the body is a string and needs parsing
+
             data = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
         } catch (error) {
             console.error('Error parsing JSON from event body:', error);
@@ -26,7 +26,6 @@ exports.handler = async (event) => {
             };
         }
     } else {
-        // Assume it's a direct invocation if no body is present
         data = event;
     }
 
@@ -40,10 +39,9 @@ exports.handler = async (event) => {
         }
     };
 
-    // Insert the event into the DynamoDB table
     try {
         await dynamodb.put(eventRecord).promise();
-        // Generate the response for successful data insertion
+
         const response = {
             statusCode: 201,
             headers: { 'Content-Type': 'application/json' },
@@ -54,7 +52,9 @@ exports.handler = async (event) => {
         console.error('Error inserting data into DynamoDB:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'An error occurred while inserting data into DynamoDB' })
+            body: JSON.stringify({ error: 'An error occurred while inserting data into DynamoDB ' + 
+                error.toISOString()
+             })
         };
     }
 };
